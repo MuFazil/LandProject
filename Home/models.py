45f548d30b4a3bccb00704cd562c32c6d0af87c2
MuFazil <mhdfazil61@gmail.com>
2024-03-03 20:24:56 +0530

    before renaming

--- Home/models.py
+++ Home/models.py
@@ -1,11 +1,24 @@
 from django.db import models
 
 class Property(models.Model):
-    owner_name = models.CharField(max_length=150)
+    # PROPERTY_CHOICES = [
+    #     ('Apartment', 'Apartment'),
+    #     ('Villa', 'Villa'),
+    #     ('Independent House', 'Independent House'),
+    #     ('Commercial Space', 'Commercial Space'),
+    #     ('Land', 'Land'),
+    # ]
+    property_name = models.CharField(max_length=100)
+    builder_name = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.TextField()
-    land_photo = models.ImageField(upload_to='property_photos/')
+    images = models.ImageField(upload_to='property_images/')
+    city_name = models.CharField(max_length=100)
+    area_range_sqft = models.IntegerField(default=0)
+    bhk_range = models.IntegerField(default=1)
+    floor_plan = models.ImageField(upload_to='floor_plans/')
+    amenities = models.JSONField(default=dict)  # Providing an empty dictionary as the default value
+    brief_about_property = models.TextField(default = 'NA')
+    about_builder = models.TextField(default = 'NA')
 
     def __str__(self):
-        return self.owner_name
+        return self.property_name
